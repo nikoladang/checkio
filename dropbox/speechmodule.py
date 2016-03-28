822113afb4b158423f98953fd1397944c7e43efd
ducthinhdt <ducthinhdt@gmail.com>
2016-03-28 14:17:28 +0700

    Speech Module challenge

--- dropbox/speechmodule.py
+++ dropbox/speechmodule.py
@@ -1,4 +1,7 @@
 __author__ = 'nikoladang'
+# http://stackoverflow.com/questions/14120340/python-error-in-basic-subtraction
+    # http://stackoverflow.com/questions/11950819/python-math-is-wrong/11950951#11950951
+# http://stackoverflow.com/questions/3886402/python-how-to-get-numbers-after-decimal-point
 
 FIRST_TEN = ["one", "two", "three", "four", "five", "six", "seven",
              "eight", "nine"]
@@ -8,11 +11,37 @@ OTHER_TENS = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy",
               "eighty", "ninety"]
 HUNDRED = "hundred"
 
+from decimal import *
+getcontext().prec = 8
 
 def checkio(number):
-
-    #replace this for solution
-    return 'string representation of n'
+    words = []
+    original = number
+    while number != 0:
+        if number < 10:
+            words.append(FIRST_TEN[int(number-1)])
+            break
+        elif number >= 10 and number <= 19:
+            whole = int(number // 10)
+            # number = math.ceil((number/10 - whole)*10)
+            number = int((Decimal(number)/10 - whole)*10)
+            words.append(SECOND_TEN[int(number)])
+            break
+        elif number == 100:
+            words.append("one "+HUNDRED)
+            break
+        elif number > 100:
+            whole = int(number // 100)
+            number = int((Decimal(number)/100 - whole)*100)
+            words.append(FIRST_TEN[whole-1]+ " hundred")
+        elif number > 10:
+            whole = int(number // 10)
+            # number = (number/10 - whole)*10
+            number = int((Decimal(number)/10 - whole)*10)
+            words.append(OTHER_TENS[whole-2])
+    result = " ".join(words)
+    print(result)
+    return " ".join(words)
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
